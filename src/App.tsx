7d78f747696762aade8959cb5ddaa2c2f9820a5d
AnthonyMrt <anthony.martial14@gmail.com>
2022-06-28 08:22:20 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { FunctionComponent } from "react";
+import PokemonList from "./pages/pokemon-list";
+import PokemonsDetail from "./pages/pokemon-detail";
+import PageNotFound from "./pages/page-not-found";
+import PokemonEdit from "./pages/pokemon-edit";
+import PokemonAdd from "./pages/pokemon-add";
+import Login from "./pages/login";
+import PrivateRoute from "./PrivateRoute";
 
-function App() {
+const App: FunctionComponent = () => {
+  //const [name, setName] = useState<String>("React");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        {/* La barre de navigation */}
+        <nav>
+          <div className="nav-wrapper teal">
+            <Link to="/" className="brand-logo-center">
+              Pokédex
+            </Link>
+          </div>
+          {/* le système de gestion des routes de l'application */}
+        </nav>
+        <Routes>
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path="/" element={<PokemonList />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path="/pokemons" element={<PokemonList />} />
+          </Route>
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path="/pokemons/:id" element={<PokemonsDetail />} />
+          </Route>
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path="/pokemon/edit/:id" element={<PokemonEdit />} />
+          </Route>
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path="/pokemon/add" element={<PokemonAdd />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
